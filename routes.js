d5dc06e155bce2554dac0c0463ad02df5aaad520
ryancasey19 <ryancasey19@gmail.com>
2020-03-26 22:33:13 -0400

    added database connectivity

--- routes.js
+++ routes.js
@@ -1,14 +1,4 @@
-//test variable
-const bromileys = [
-    {
-        id: 1,
-        name: "Brongiley"
-    },
-    {
-        id: 2,
-        name: "Vroooooomiley"
-    }
-];
+const pool = require('./data/config');
 
 const router = app => {
     app.get('/', (request, response) => {
@@ -17,8 +7,19 @@ const router = app => {
         });
     });
     app.get('/brom', (request, response) => {
-        response.send(bromileys);
+        pool.query('SELECT * FROM bromileys', (error, result) => { 
+            if (error) throw error;
+            response.send(result);
+        });
     });
+    //display a single brom by id
+    app.get('/brom/:id', (request, response) => {
+        const id = request.params.id;
+        pool.query('SELECT * FROM bromileys WHERE id = ?', id, (error, result) => {
+            if (error) throw error;
+            response.send(result);
+        })
+    })
 }
 
 //export the router
